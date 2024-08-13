a3884e4ae5cef4878061f9a5c7f5ba3781200a83
pikaparson <106161317+pikaparson@users.noreply.github.com>
2024-08-13 11:13:38 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,62 +34,15 @@ void main() async {
 }
 
 
-import os
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QAction, QMessageBox
-from qgis.core import QgsProject, QgsPointXY, QgsGeometry
-from qgis.gui import QgsMapToolEmitPoint
-from .distance_calculator_dialog import DistanceCalculatorDialog
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog, QPushButton, QLabel
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'distance_calculator_dialog_base.ui'))
 
 
-class DistanceCalculator:
-    def __init__(self, iface):
-        self.iface = iface
-        self.plugin_dir = os.path.dirname(__file__)
-        self.toolbar = self.iface.addToolBar('DistanceCalculator')
-        self.toolbar.setObjectName('DistanceCalculator')
-
-        self.action = QAction('Calculate Distance', self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
-        self.toolbar.addAction(self.action)
-
-        self.canvas = self.iface.mapCanvas()
-        self.tool = QgsMapToolEmitPoint(self.canvas)
-        self.tool.canvasClicked.connect(self.display_point)
-
-        self.points = []
-
-    def initGui(self):
-        pass
-
-    def display_point(self, point, button):
-        if len(self.points) < 2:
-            self.points.append(point)
-        if len(self.points) == 2:
-            QMessageBox.information(self.iface.mainWindow(), 'Info', "Two points selected")
-
-    def run(self):
-        self.points = []
-        self.canvas.setMapTool(self.tool)
-        self.dialog = DistanceCalculatorDialog()
-        self.dialog.calculateButton.clicked.connect(self.calculate_distance)
-        self.dialog.show()
-
-    def calculate_distance(self):
-        if len(self.points) != 2:
-            QMessageBox.warning(self.iface.mainWindow(), 'Error', 'Please select exactly two points.')
-            return
-
-        point1 = QgsPointXY(self.points[0].x(), self.points[0].y())
-        point2 = QgsPointXY(self.points[1].x(), self.points[1].y())
-
-        distance = point1.distance(point2)
-        self.dialog.resultLabel.setText(f'Distance: {distance:.2f} units')
-
-    def unload(self):
-        self.toolbar.removeAction(self.action)
-
-
-def classFactory(iface):
-    return DistanceCalculator(iface)
- 
+class DistanceCalculatorDialog(QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(DistanceCalculatorDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.calculateButton = self.findChild(QPushButton, 'calculateButton')
+        self.resultLabel = self.findChild(QLabel, 'resultLabel')
