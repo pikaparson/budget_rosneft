d5b700171c083eb9b4f20815236e43a00545f18d
pikaparson <106161317+pikaparson@users.noreply.github.com>
2024-08-14 14:04:53 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,174 +33,87 @@ void main() async {
   ));
 }
 
+метадата
+[general]
+name=DistanceCalculator
+description=Плагин для расчета расстояния между двумя точками
+version=0.1
+qgisMinimumVersion=3.22
+author=Ваше имя
+email=ваш_email@example.com
+category=Spatial Analysis
 
-__init__.py:def classFactory(iface):
-from .DISTANCE_CALCULATOR import DistanceCalculatorPlugin
-return DistanceCalculatorPlugin(iface)
-DISTANCE_CALCULATOR.py:from qgis.core import QgsProject, QgsPointXY, QgsFeature
-from qgis.gui import QgsMapCanvas
-from qgis.PyQt.QtWidgets import QAction, QMessageBox
-from .DISTANCE_CALCULATOR_dialog import DistanceCalculatorDialog
-class DistanceCalculatorPlugin:
-def __init__(self, iface):
-self.iface = iface
-self.canvas = iface.mapCanvas()
-self.dlg = None
-def initGui(self):
-icon_path = ':/plugins/distance_calculator/icon.png'
-self.action = QAction('Calculate Distance', self.iface.mainWindow())
-self.action.triggered.connect(self.run)
-self.iface.addToolBarIcon(self.action)
-self.iface.addPluginToMenu('Distance Calculator', self.action)
-def unload(self):
-self.iface.removePluginMenu('Distance Calculator', self.action)
-self.iface.removeToolBarIcon(self.action)
-def run(self):
-if self.dlg is None:
-  self.dlg = DistanceCalculatorDialog()
-  self.dlg.calculateButton.clicked.connect(self.calculate_distance)
-self.dlg.show()
-self.dlg.exec_()
-def calculate_distance(self):
-selected_features = self.get_selected_features()
-if len(selected_features) != 2:
-QMessageBox.warning(self.dlg, "Ошибка", "Пожалуйста, выберите ровно две точки.")
-return
-point1 = selected_features[0].geometry().asPoint()
-point2 = selected_features[1].geometry().asPoint()
-distance = point1.distance(point2)
-self.dlg.resultLabel.setText(f"Расстояние: {distance:.2f} метров")
-def get_selected_features(self):
-layers = QgsProject.instance().mapLayers().values()
-selected_features = []
-for layer in layers:
-if layer.selectedFeatureCount() > 0:
-selected_features.extend(layer.selectedFeatures())
-return selected_features
-DISTANCE_CALCULATOR_dialog.py
-from qgis.PyQt import QtWidgets, uic
-import os
-FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'DISTANCE_CALCULATOR_dialog_base.ui'))
-class DistanceCalculatorDialog(QtWidgets.QDialog, FORM_CLASS):
-    def __init__(self, parent=None):
-        super(DistanceCalculatorDialog, self).__init__(parent)
-        self.setupUi(self)
-DISTANCE_CALCULATOR_dialog_base.ui
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
-<class>DistanceCalculatorDialogBase</class>
-<widget class="QDialog" name="DistanceCalculatorDialogBase">
-<property name="geometry">
-<rect>
-<x>0</x>
-<y>0</y>
-<width>400</width>
-<height>300</height>
-</rect>
-</property>
-<property name="windowTitle">
-<string>Distance Calculator</string>
-</property>
-<layout class="QVBoxLayout" name="verticalLayout">
-<item>
-<widget class="QWidget" name="formLayoutWidget">
-<layout class="QFormLayout" name="formLayout">
-<property name="fieldGrowthPolicy">
-<enum>QFormLayout::AllNonFixedFieldsGrow</enum>
-</property>                      
-<item row="0" column="0">
-<widget class="QLabel" name="labelPoint1">
-<property name="text">
-<string>Point 1 Coordinates</string>
-</property>
-</widget>
-</item>
-<item row="0" column="1">
-<widget class="QLineEdit" name="lineEditPoint1">
-<property name="placeholderText">
-<string>format: x,y</string>
-</property>
-</widget>
-</item>
-<item row="1" column="0">
-<widget class="QLabel" name="labelPoint2">
-<property name="text">
-<string>Point 2 Coordinates</string>
-</property>
-</widget>
-</item>
-<item row="1" column="1">
-<widget class="QLineEdit" name="lineEditPoint2">
-<property name="placeholderText">
-<string>format: x,y</string>
-</property>
-</widget>
-</item>                        
-<item row="2" column="0" colspan="2">
-<layout class="QHBoxLayout" name="horizontalLayout">
-<property name="spacing">
-<number>6</number>
-</property>
-<property name="sizeConstraint">
-<enum>QLayout::SetDefaultConstraint</enum>
-</property>
-<item>
-<spacer name="horizontalSpacer">
-<property name="orientation">
-<enum>Qt::Horizontal</enum>
-</property>
-<property name="sizeType">
-<enum>QSizePolicy::Expanding</enum>
-</property>
-<property name="minimumSize">
-<size>
-<width>20</width>
-<height>20</height>
-</size>
-</property>
-</spacer>
-</item>
-<item>
-<widget class="QPushButton" name="buttonCalculate">
-<property name="text">
-<string>Calculate</string>
-</property>
-</widget>
-</item>
-<item>
-<spacer name="horizontalSpacer_2">
-<property name="orientation">
-<enum>Qt::Horizontal</enum>
-</property>
-<property name="sizeType">
-<enum>QSizePolicy::Expanding</enum>
-</property>
-<property name="minimumSize">
-<size>
-<width>20</width>
-<height>20</height>
-</size>
-</property>
-</spacer>
-</item>
-</layout>
-</item>
-<item row="3" column="0" colspan="2">
-<widget class="QLabel" name="labelResult">
-<property name="text">
-<string/>
-</property>
-<property name="alignment">
-<set>Qt::AlignCenter</set>
-</property>
-</widget>
-</item>
-</layout>
-</widget>
-</item>
-</layout>
-</widget>
-<resources/>
-<connections/>
-</ui> 
+[python]
+type=python
+
+  инит
+  def classFactory(iface):
+    from .main import DistanceCalculator
+    return DistanceCalculator(iface)
+
+ маин
+      from PyQt5 import QtWidgets
+from qgis.core import QgsProject, QgsFeature
+from qgis.gui import QgsMapToolEmitPoint
+from qgis.PyQt.QtCore import Qt
+
+class DistanceCalculator:
+    def __init__(self, iface):
+        self.iface = iface
+        self.canvas = self.iface.mapCanvas()
+        self.dlg = DistanceCalculatorDialog()
+        self.points = []
+
+        # Обработчики событий
+        self.dlg.buttonCalculate.clicked.connect(self.calculate_distance)
+        self.dlg.buttonSelect.clicked.connect(self.select_points)
+
+    def show(self):
+        self.dlg.show()
+        self.dlg.raise_()
+        self.dlg.activateWindow()
+
+    def select_points(self):
+        self.points = []
+        self.tool = QgsMapToolEmitPoint(self.canvas)
+        self.tool.canvasClicked.connect(self.add_point)
+        self.canvas.setMapTool(self.tool)
+
+    def add_point(self, point):
+        self.points.append(point)
+        if len(self.points) == 2:
+            self.canvas.unsetMapTool(self.tool)
+
+    def calculate_distance(self):
+        if len(self.points) != 2:
+            QtWidgets.QMessageBox.warning(self.dlg, "Ошибка", "Пожалуйста, выберите две точки.")
+            return
+
+        distance = self.points[0].distance(self.points[1])
+        self.dlg.labelResult.setText(f"Расстояние: {distance:.2f} метров")
+
+class DistanceCalculatorDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Калькулятор расстояния")
+        self.setGeometry(100, 100, 300, 150)
+
+        # Элементы интерфейса
+        self.buttonCalculate = QtWidgets.QPushButton("Расчитать расстояние", self)
+        self.buttonCalculate.setGeometry(50, 70, 200, 30)
+
+        self.buttonSelect = QtWidgets.QPushButton("Выбрать точки", self)
+        self.buttonSelect.setGeometry(50, 30, 200, 30)
+
+        self.labelResult = QtWidgets.QLabel("Расстояние: ", self)
+        self.labelResult.setGeometry(50, 110, 200, 30)
+
+ресоурс кср
+ <RCC>
+  <qresource prefix="/your_plugin">
+  </qresource>
+</RCC>
+
+
+ресорс пай через баш
+  pyrcc5 resources.qrc -o resources.py
