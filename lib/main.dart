1f4333bd1355729834508f5101253d55143626f3
pikaparson <106161317+pikaparson@users.noreply.github.com>
2024-08-14 11:34:16 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,97 +34,95 @@ void main() async {
 }
 
 
-import os
-from qgis.core import QgsPointXY, QgsWkbTypes, QgsProject, QgsFeature
-from qgis.utils import iface
-from qgis.gui import QgsMapToolEmitPoint
-from PyQt5.QtWidgets import QAction, QDialog, QVBoxLayout, QLabel, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.uic import loadUi
 
+
+from qgis.PyQt.QtCore import *
+from qgis.PyQt.QtWidgets import *
+from qgis.core import *
+from qgis.utils import iface
 
 class DistanceCalculatorPlugin:
+
     def __init__(self, iface):
         self.iface = iface
-        self.canvas = iface.mapCanvas()
-        self.plugin_dir = os.path.dirname(__file__)
-        self.first_point = None
-        self.second_point = None
-        self.mapTool = None
-        self.dialog = None
+        self.action = QAction("Calculate Distance", self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
 
     def initGui(self):
-        icon_path = os.path.join(self.plugin_dir, 'icon.png')
-        self.action = QAction(QIcon(icon_path), 'Calculate Distance', self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
+        self.iface.addPluginToMenu("Calculate Distance", self.action)
         self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu('Calculate Distance', self.action)
 
     def unload(self):
+        self.iface.removePluginMenu("Calculate Distance", self.action)
         self.iface.removeToolBarIcon(self.action)
-        self.iface.removePluginMenu('Calculate Distance', self.action)
-
+    
     def run(self):
-        # Создаем и показываем диалог
         self.dialog = DistanceCalculatorDialog(self.iface)
-        self.dialog.selectFirstPointButton.clicked.connect(self.select_first_point)
-        self.dialog.selectSecondPointButton.clicked.connect(self.select_second_point)
         self.dialog.show()
 
-    def select_first_point(self):
-        self.mapTool = QgsMapToolEmitPoint(self.canvas)
-        self.canvas.setMapTool(self.mapTool)
-        self.mapTool.canvasClicked.connect(self.set_first_point)
-
-    def set_first_point(self, point):
-        self.first_point = point
-        self.dialog.firstPointLabel.setText(f'First Point: {point.x()}, {point.y()}')
-        self.reset_map_tool()
-
-    def select_second_point(self):
-        self.mapTool = QgsMapToolEmitPoint(self.canvas)
-        self.canvas.setMapTool(self.mapTool)
-        self.mapTool.canvasClicked.connect(self.set_second_point)
-
-    def set_second_point(self, point):
-        self.second_point = point
-        self.dialog.secondPointLabel.setText(f'Second Point: {point.x()}, {point.y()}')
-        self.reset_map_tool()
-        self.calculate_distance()
-
-    def calculate_distance(self):
-        if self.first_point and self.second_point:
-            distance = self.first_point.distance(self.second_point)
-            self.dialog.resultLabel.setText(f'Distance: {distance} map units')
-
-    def reset_map_tool(self):
-        self.canvas.unsetMapTool(self.mapTool)
-        self.mapTool.deleteLater()
-        self.mapTool = None
-
-
 class DistanceCalculatorDialog(QDialog):
-    def __init__(self, iface, parent=None):
-        super().__init__(parent)
+
+    def __init__(self, iface):
+        QDialog.__init__(self)
         self.iface = iface
-        self.setWindowTitle('Distance Calculator')
-        self.setGeometry(100, 100, 200, 150)
+        self.setWindowTitle("Calculate Distance")
+        self.setGeometry(100, 100, 300, 100)
+
+        self.layerComboBox1 = QComboBox()
+        self.layerComboBox2 = QComboBox()
+        self.pointComboBox1 = QComboBox()
+        self.pointComboBox2 = QComboBox()
+        
+        self.calculateButton = QPushButton("Calculate Distance")
+        self.calculateButton.clicked.connect(self.calculate_distance)
 
         layout = QVBoxLayout()
-
-        self.firstPointLabel = QLabel('First Point: Not Set')
-        layout.addWidget(self.firstPointLabel)
-
-        self.selectFirstPointButton = QPushButton('Select First Point')
-        layout.addWidget(self.selectFirstPointButton)
-
-        self.secondPointLabel = QLabel('Second Point: Not Set')
-        layout.addWidget(self.secondPointLabel)
-
-        self.selectSecondPointButton = QPushButton('Select Second Point')
-        layout.addWidget(self.selectSecondPointButton)
-
-        self.resultLabel = QLabel('Distance: N/A')
-        layout.addWidget(self.resultLabel)
-
+        layout.addWidget(QLabel("Select layer for first point:"))
+        layout.addWidget(self.layerComboBox1)
+        layout.addWidget(QLabel("Select first point:"))
+        layout.addWidget(self.pointComboBox1)
+        layout.addWidget(QLabel("Select layer for second point:"))
+        layout.addWidget(self.layerComboBox2)
+        layout.addWidget(QLabel("Select second point:"))
+        layout.addWidget(self.pointComboBox2)
+        layout.addWidget(self.calculateButton)
+        
         self.setLayout(layout)
+
+        self.populate_layers()
+        self.layerComboBox1.currentIndexChanged.connect(self.populate_points1)
+        self.layerComboBox2.currentIndexChanged.connect(self.populate_points2)
+    
+    def populate_layers(self):
+        layers = [layer for layer in QgsProject.instance().mapLayers().values() if isinstance(layer, QgsVectorLayer) and layer.geometryType() == QgsWkbTypes.PointGeometry]
+        for layer in layers:
+            self.layerComboBox1.addItem(layer.name(), layer)
+            self.layerComboBox2.addItem(layer.name(), layer)
+        
+    def populate_points1(self):
+        self.pointComboBox1.clear()
+        layer = self.layerComboBox1.currentData()
+        if layer:
+            features = layer.getFeatures()
+            for feature in features:
+                self.pointComboBox1.addItem(str(feature.id()), feature.geometry().asPoint())
+    
+    def populate_points2(self):
+        self.pointComboBox2.clear()
+        layer = self.layerComboBox2.currentData()
+        if layer:
+            features = layer.getFeatures()
+            for feature in features:
+                self.pointComboBox2.addItem(str(feature.id()), feature.geometry().asPoint())
+    
+    def calculate_distance(self):
+        point1 = self.pointComboBox1.currentData()
+        point2 = self.pointComboBox2.currentData()
+        if point1 and point2:
+            distance = point1.distance(point2)
+            QMessageBox.information(self, "Distance", f"Distance: {distance:.2f} meters")
+        else:
+            QMessageBox.warning(self, "Error", "Please select both points")
+
+def classFactory(iface):
+    return DistanceCalculatorPlugin(iface)
